4a33c3bc7e0d94363937c3d3f809db2283e81148
subhadip1203 <subhadip1203@gmail.com>
2021-08-29 00:43:17 +0530

    remove test route

--- Micro-ORM/app/index.js
+++ Micro-ORM/app/index.js
@@ -14,10 +14,6 @@ app.get('/', async function (req, res) {
     const ping = { status: 'live', time: new Date().getTime() };
     res.status(200).send(ping);
 });
-app.get('/test', async function (req, res) {
-    const ping = { status: 'test', time: new Date().getTime() };
-    res.status(200).send(ping);
-});
 
 /*===========================================================================
 ---------------------------------  Run Server - -----------------------------
