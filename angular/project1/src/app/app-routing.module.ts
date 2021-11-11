e2e70fcf08e42d6123f5f57515fc9f2075b1a311
subhadip1203 <subhadip1203@gmail.com>
2021-11-11 23:24:19 +0530

    angular route

--- angular/project1/src/app/app-routing.module.ts
+++ angular/project1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutusComponent} from './aboutus/aboutus.component'
+import {ContactusComponent} from './contactus/contactus.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutusComponent },
+  { path: 'contactus', component: ContactusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/project1/src/app/app.module.ts
+++ angular/project1/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
