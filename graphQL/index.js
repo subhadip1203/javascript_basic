821b7d974d2dc64adf0e86f0eb6c565c5b8f9243
subhadip1203 <subhadip1203@gmail.com>
2021-12-22 01:07:50 +0530

    graphql basic 2

--- graphQL/index.js
+++ graphQL/index.js
@@ -1,1 +1,26 @@
-// start project index page
+const { ApolloServer , gql } = require('apollo-server');
+
+const typeDefs = gql `
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers =  {
+  Query : {
+    hello : ()=> {
+      return 'hello world'
+    }
+  }
+}
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+
+server.listen()
+.then( r => console.log(`server is running at : ${r.url}`) )
+.catch( err => console.log(err) )
