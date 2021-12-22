956a2fa174d931805a31c227585249d95b6956a2
subhadip1203 <subhadip1203@gmail.com>
2021-12-23 02:12:58 +0530

    graphQl basic 3

--- graphQL/index.js
+++ graphQL/index.js
@@ -3,6 +3,10 @@ const { ApolloServer , gql } = require('apollo-server');
 const typeDefs = gql `
   type Query {
     hello: String
+    mandetoryField: String!
+    numberOfCount: Int
+    price: Float
+    isActive: Boolean
   }
 `
 
@@ -10,6 +14,19 @@ const resolvers =  {
   Query : {
     hello : ()=> {
       return 'hello world'
+    },
+    mandetoryField: ()=>{
+      return 'it is a must'
+      //return null // will through error as not-nullable field
+    },
+    numberOfCount: ()=>{
+      return 10;
+    },
+    price: ()=>{
+      return 99.99
+    },
+    isActive: ()=>{
+      return true
     }
   }
 }
