2958c28682ce1de339cf701c3ad70b6780ed7f47
subhadip1203 <subhadip1203@gmail.com>
2021-12-24 11:06:18 +0530

    graphQL composite typedefs

--- graphQL/index.js
+++ graphQL/index.js
@@ -1,16 +1,31 @@
 const { ApolloServer , gql } = require('apollo-server');
 
 const typeDefs = gql `
+
+  type Product {
+    name: String !
+    description: String!
+    price: Float!
+    quantity: Int!
+    isActive: Boolean
+  }
+  
+
   type Query {
     hello: String
     mandetoryField: String!
     numberOfCount: Int
     price: Float
     isActive: Boolean
+    myStringArr: [String]
+    noNullStringArr: [String!]!
+    products: [Product!]!
   }
 `
 
 const resolvers =  {
+
+  
   Query : {
     hello : ()=> {
       return 'hello world'
@@ -27,7 +42,32 @@ const resolvers =  {
     },
     isActive: ()=>{
       return true
-    }
+    },
+    myStringArr: ()=>{
+      return ['hi', null, 'hope', 'all', 'good']
+    },
+    noNullStringArr: ()=>{
+      return ['hi', 'hope', 'all', 'good']
+    },
+
+    products: ()=>{
+      return [
+        {
+          name: 'product 1',
+          description: 'product description 1',
+          price: 99.99,
+          quantity: 10,
+          isActive: true
+        },
+        {
+          name: 'product 2',
+          description: 'product description 2',
+          price: 199.99,
+          quantity: 20,
+          isActive: false
+        }
+      ]
+    },
   }
 }
 
