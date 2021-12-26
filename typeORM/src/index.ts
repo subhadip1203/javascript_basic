b5204a0e5dde0793ecf92d90af2a314e80ca56b1
subhadip1203 <subhadip1203@gmail.com>
2021-12-26 23:42:59 +0530

    typeORM with express

--- typeORM/src/index.ts
+++ typeORM/src/index.ts
@@ -1,22 +1,40 @@
+import express = require('express');
+const app = express()
+const PORT = 3000
+
+
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./typeorm/entity/User";
 
-createConnection().then(async connection => {
+const mainFunc = async () =>{
+    try{
+        await createConnection();
+        console.log("DB connection is successful")
+        app.listen( PORT , () =>  console.log("app running http://localhost:"+PORT) )
+    }
+    catch(err){
+        console.log(err)
+    }
+}
 
-    console.log("connection is successful")
-    // console.log("Inserting a new user into the database...");
-    // const user = new User();
-    // user.firstName = "Timber";
-    // user.lastName = "Saw";
-    // user.age = 25;
-    // await connection.manager.save(user);
-    // console.log("Saved a new user with id: " + user.id);
+mainFunc()
 
-    // console.log("Loading users from the database...");
-    // const users = await connection.manager.find(User);
-    // console.log("Loaded users: ", users);
+// createConnection().then(async connection => {
 
-    //console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log("connection is successful")
+//     // console.log("Inserting a new user into the database...");
+//     // const user = new User();
+//     // user.firstName = "Timber";
+//     // user.lastName = "Saw";
+//     // user.age = 25;
+//     // await connection.manager.save(user);
+//     // console.log("Saved a new user with id: " + user.id);
 
-}).catch(error => console.log(error));
+//     // console.log("Loading users from the database...");
+//     // const users = await connection.manager.find(User);
+//     // console.log("Loaded users: ", users);
+
+//     //console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
