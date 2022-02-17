92107a405c64ac4e17a459563cedc26780b09ff5
subhadip1203 <subhadip1203@gmail.com>
2022-02-17 23:54:44 +0530

    select drop down

--- vue-bootstrap/src/components/HelloWorld.vue
+++ vue-bootstrap/src/components/HelloWorld.vue
@@ -11,6 +11,16 @@
 
     <button type="button" class="btn btn-link">Link</button>
   </div>
+  <div class="row">
+    <select class="form-select" aria-label="Default select example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div>
+ 
+
 </template>
 
 <script>
