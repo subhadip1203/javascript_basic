7833ac14ca7cb33bfe433f2ebfc95f4ca173bd14
subhadip1203 <subhadip1203@gmail.com>
2022-06-13 23:52:33 +0530

    JS class-obj 1

---
+++ calss_objects/Parent.class.js
@@ -0,0 +1,15 @@
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+
+	greet() {
+		console.log(`Hello ${this.name}`);
+	}
+}
+
+// inheriting parent class
+class Student extends Person {}
+
+let student1 = new Student("Jack");
+student1.greet();
